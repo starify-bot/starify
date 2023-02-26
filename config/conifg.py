c297175afb9bb9c3c450ad72f8e1ec466a0da61d
Juwen Nagpal <Juwennagpal6@gmail.com>
2023-02-26 18:28:09 +0100

    Updated some files

--- config/conifg.py
+++ config/conifg.py
@@ -1,4 +1,4 @@
 TOKEN = "" # Discord Bot Token
 PREFIX_DEV = "//" # Prefix for development
-CLIENT_ID = 0 # Discord Client ID
+CLIENT_ID = 1079442018943643678 # Discord Client ID
 EMBED_COLOR = 0xFFDE59 # Embed Color
--- src/cluster.py
+++ src/cluster.py
@@ -8,6 +8,11 @@ intents.reactions = True
 intents.message_content = True
 intents.messages = True
 
+activity = discord.Streaming(
+    name="/help",
+    url="https://www.twitch.tv/starify"
+)
+
 ShardManager = commands.AutoShardedBot(
     command_prefix=PREFIX_DEV,
     help_command=None,
@@ -15,4 +20,14 @@ ShardManager = commands.AutoShardedBot(
 )
 
 async def shard_manager_load_extensions():
-    for 
+    for filname in os.listdir("cogs"):
+        if filname.endswith(".py"):
+            ShardManager.load_extension(f"cogs.{filname[:-3]}")
+            print(f"{Fore.GREEN}Loaded {filname[:-3]}{Style.RESET_ALL}")
+            
+@ShardManager.event
+async def on_ready():
+    print(f"{Fore.GREEN}Shard Manager is online!{Style.RESET_ALL}")
+    await shard_manager_load_extensions()
+    
+ShardManager.run(token=TOKEN, reconnect=True)
